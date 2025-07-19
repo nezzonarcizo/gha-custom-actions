ecabdf413d1b67aa147c282f5b10742bbc776f44
Marlon Augusto <nesso.narcizo@hotmail.com>
2025-07-19 16:50:18 -0400

    fix: getInput() method syntax

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -4,9 +4,9 @@ const exec = require('@actions/exec');
 
 function run() {
     // 1) Get some input values
-    const bucket = core.getInpu('bucket', { required: true });
-    const bucketRegion = core.getInpu('bucket-region', { required: true });
-    const distFolder = core.getInpu('dist-folder', { required: true });
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
 
     // 2) Upload files
     const s3Uri = `s3://${bucket}`
