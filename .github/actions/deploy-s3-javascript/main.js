5f206f78e13bb397365a8e411c09ae0deeb89314
Marlon Augusto <nesso.narcizo@hotmail.com>
2025-07-19 17:24:21 -0400

    fix: s3 website endpoint

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -12,7 +12,7 @@ function run() {
     const s3Uri = `s3://${bucket}`
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`) // AWS CLI is already on 'ubuntu-latest' image where Actions runs
 
-    const websiteUrl = `http://${bucket}.s3.${bucketRegion}.amazonaws.com`;
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
     core.setOutput('website-url', websiteUrl); // set-output
 }
 
